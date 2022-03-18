26afc85dde91f7012c419ebff7068216c5844b3e
Susan Cheng <susan.doggie@gmail.com>
2022-03-18 16:57:18 +0800

    test

---
+++ test/test.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const { test } = require('uvu')
+const expect = require('expect')
+const Mongoose = require('mongoose')
+
+const { 
+    MONGO_USERNAME, 
+    MONGO_PASSWORD, 
+    MONGO_DATABASE,
+     MONGO_REPLICA_SET,
+} = process.env
+
+test.before(async () => {
+
+  const connectionString = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@localhost:${27017}/${MONGO_DATABASE}?replicaSet=${MONGO_REPLICA_SET}`
+
+  console.log('---------------------------------------------------------------------')
+  console.log('connecting to MongoDB using connection string -> ' + connectionString)
+  console.log('---------------------------------------------------------------------')
+
+  await Mongoose.connect(connectionString, {
+    serverSelectionTimeoutMS: 1500
+  })
+})
+
+test.after(async () => {
+  await Mongoose.connection.db.dropDatabase()
+  await Mongoose.disconnect()
+})
+
+test('queries the replica set status', async () => {
+  const db = Mongoose.connection.db.admin()
+  const { ok, set } = await db.command({ replSetGetStatus: 1 })
+
+  expect(ok).toBe(1)
+  expect(set).toEqual(MONGO_REPLICA_SET)
+})
+
+test('saves a document', async () => {
+  const Dog = Mongoose.model('Dog', { name: String })
+  const albert = await new Dog({ name: 'Albert' }).save()
+  expect(albert.name).toEqual('Albert')
+})
+
+test('uses transactions', async () => {
+  const Customer = Mongoose.model('Customer', new Mongoose.Schema({ name: String }))
+  await Customer.createCollection()
+
+  const session = await Mongoose.startSession()
+  session.startTransaction()
+
+  await Customer.create([{ name: 'test-customer' }], { session })
+
+  expect(
+    await Customer.findOne({ name: 'test-customer' })
+  ).toBeNull()
+
+  expect(
+    await Customer.findOne({ name: 'test-customer' }).session(session)
+  ).not.toBeNull()
+
+  await session.commitTransaction()
+
+  expect(
+    await Customer.findOne({ name: 'test-customer' })
+  ).not.toBeNull()
+})
+
+test.run()
